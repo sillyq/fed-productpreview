76950a88804496c7a2866a94f25706f4b11c1271
Qwerytx_ <potatoloadzch@gmail.com>
2024-05-19 01:04:35 +0700

    supported small screen (but not big screen)

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      playfair: ["Playfair Display"],
+      inter: ["Inter"],
+      sche: ["Tai Heritage Pro"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
